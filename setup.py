a375ea3348b8a74fcae71e90ffb8e3397b5360f1
techsavvyomi <omi007dandekar@gmail.com>
2026-03-24 14:43:25 +0530

    chore: bump version to 2.0.0 for major release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='plutocontrol',
-    version='0.2.2',
+    version='2.0.0',
     packages=find_packages(),
     install_requires=[],
     author='Omkar Dandekar',
